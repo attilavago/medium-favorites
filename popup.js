4afc6fc1dfa484ecfefdff9602c225596ac2228c
Attila Vago <attilavago85@gmail.com>
2022-10-15 20:34:55 +0100

    ahow images in authors popup

--- popup.js
+++ popup.js
@@ -3,10 +3,16 @@ const authorsListElement = document.getElementById("authorsList");
 const addToAuthors = (author) => {
     let li = document.createElement('li');
     let link = document.createElement('a');
+    let image = document.createElement('img');
+    let authorName = document.createElement('p');
+    image.src = author.profileImg;
+    image.alt = author.name;
+    authorName.textContent = author.name;
     link.href = author.url;
-    link.textContent = author.name;
     link.target = "_blank";
     link.rel = "noreferrer noopener";
+    link.appendChild(image);
+    link.appendChild(authorName);
     li.appendChild(link);
     authorsListElement.appendChild(li);
 }
